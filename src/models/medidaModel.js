eca584bc66f95230150a2d6d1e070e7fa3e26f3c
carolinamontagner <carolina.jaouiche@sptech.school>
2024-06-02 06:23:53 -0300

    Mexendo na dash

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,15 +1,22 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+var instrucaoSql= `SELECT acertos, COUNT(fkusuario) AS quantidade_usuarios
+FROM respostas
+WHERE fk_usuario = ${idAquario}
+GROUP BY acertos
+;`
+    
+    
+    
+    // var instrucaoSql = `SELECT 
+    //     dht11_temperatura as temperatura, 
+    //     dht11_umidade as umidade,
+    //                     momento,
+    //                     DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+    //                 FROM medida
+    //                 WHERE fk_aquario = ${idAquario}
+    //                 ORDER BY id DESC LIMIT ${limite_linhas}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -17,13 +24,18 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
 
 function buscarMedidasEmTempoReal(idAquario) {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+    var instrucaoSql= `SELECT acertos, COUNT(fkusuario) AS quantidade_usuarios
+FROM respostas
+WHERE fk_usuario = ${idAquario}
+GROUP BY acertos;
+    
+
+    //     dht11_temperatura as temperatura, 
+    //     dht11_umidade as umidade,
+    //                     DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+    //                     fk_aquario 
+    //                     FROM medida WHERE fk_aquario = ${idAquario} 
+    //                 ORDER BY id DESC LIMIT 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
